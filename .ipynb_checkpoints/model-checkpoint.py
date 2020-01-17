278cafbb6f0b4c231409eec2a9b80136c3730d8e
Cirhuza Alain <cirhuzalain@gmail.com>
2020-01-17 10:50:45 +0000

    Add CNN2

--- .ipynb_checkpoints/model-checkpoint.py
+++ .ipynb_checkpoints/model-checkpoint.py
@@ -24,3 +24,30 @@ class CNN(nn.Module):
         x = self.fc2(x)
         x = F.log_softmax(x, dim=1)
         return x
+    
+class CNN2(nn.Module):
+    def __init__(self, input_size, n_feature, output_size):
+        super(CNN2, self).__init__()
+        self.n_feature = n_feature
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=n_feature, kernel_size=5)
+        self.conv2 = nn.Conv2d(n_feature, n_feature, kernel_size=5)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(n_feature*53*53, 50)
+        self.fc2 = nn.Linear(50, 2)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.dropout1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = x.view(-1, self.n_feature*53*53)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+        return x
