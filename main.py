71b2b2a51360dc36755c10378022a4267272b458
Cirhuza Alain <cirhuzalain@gmail.com>
2020-01-23 00:22:19 +0000

    Update tensorboard experiment content

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn as nn
+import torchvision
 import numpy as np
 import pandas as pd
 import os
@@ -9,12 +10,22 @@ import argparse
 
 from model import CNN1, CNN2
 from dataset import MyDataset
-from trainer import train, test
+from trainer import train, test, matplotlib_imshow
 from pathlib import Path
 from torchvision import datasets, models, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.optim import lr_scheduler
+from torch.utils.tensorboard import SummaryWriter
 from matplotlib import pyplot as plt
+
+def select_n_random(data, labels, n=100):
+    '''
+    Selects n random datapoints and their corresponding labels from a dataset
+    '''
+    assert len(data) == len(labels)
+
+    perm = torch.randperm(len(data))
+    return data[perm][:n], labels[perm][:n]
 
 
 def main():
@@ -24,6 +35,7 @@ def main():
                         type=str, default='/home/aims/Dropbox/AMMI/Tutorial/NN_1/project/Cat_Dog_data', 
                         help='Folder that contain your training and testing data')
     args = parser.parse_args()
+    writer = SummaryWriter('runs/catvsdog_experiment_1')
     
     # set path
     PATH=Path(args.data)
@@ -38,6 +50,8 @@ def main():
     output_size = 2
 
     # create custom tranform
+    viz_transforms = transforms.Compose([transforms.Resize((224,224)),
+                                      transforms.ToTensor()])
     train_transforms = transforms.Compose([transforms.Resize((224,224)),
                                           transforms.ToTensor(),
                                           transforms.Normalize([0.485, 0.456, 0.406],
@@ -48,20 +62,52 @@ def main():
                                           transforms.Normalize([0.485, 0.456, 0.406],
                                                                [0.229, 0.224, 0.225])])
     # Loading the data using custom data loader
+    viz_data = MyDataset(TRAIN, transform=viz_transforms)
     train_data = MyDataset(TRAIN, transform=train_transforms)
     valid_data = MyDataset(VALID,transform=valid_transforms)
 
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers,shuffle=True)
     valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size,  num_workers=num_workers,shuffle=True)
-
+    
+    viz_loader = torch.utils.data.DataLoader(viz_data, batch_size=batch_size, num_workers=num_workers,shuffle=True)
 
     # Create model
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
     model_cnn = CNN2(input_size, n_features, output_size)
-    optimizer = optim.SGD(model_cnn.parameters(), lr=0.01, momentum=0.5)
+    optimizer = optim.Adam(model_cnn.parameters(), lr=0.01)
+    
+    # get some random training images
+    dataiter = iter(viz_loader)
+    images, labels = dataiter.next()
 
-    for epoch in range(5):
-        train(epoch, model_cnn, train_loader, optimizer)
-        test(model_cnn, valid_loader)
+    # create grid of images
+    img_grid = torchvision.utils.make_grid(images)
+
+    # show images
+    matplotlib_imshow(img_grid)
+
+    # write to tensorboard
+    writer.add_image('four_dogvscats', img_grid)
+    
+    writer.add_graph(model_cnn, images)
+
+    # get the class labels for each image
+    classes  = ('cat', 'dog')
+    class_labels = [classes[lab] for lab in labels]
+
+    # log embeddings
+    features = images.view(-1, 224 * 224 * 3)
+    writer.add_embedding(features,
+                         metadata=class_labels,
+                         label_img=images)
+    
+    model_cnn = model_cnn.to(device)
+
+    for epoch in range(20):
+        train(epoch, model_cnn, train_loader, optimizer, writer, device)
+        test(model_cnn, valid_loader, device)
+    writer.close()
 
 if __name__ == '__main__':
     main()
--- trainer.py
+++ trainer.py
@@ -1,26 +1,85 @@
+import torch
 import torch.nn.functional as F
+from matplotlib import pyplot as plt
+import numpy as np
 from tqdm import tqdm
 
-def train(epoch, model, train_loader, optimizer):
-    model.train()
-    for batch_idx, (data, target) in tqdm(enumerate(train_loader)):
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.cpu().numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
+def plot_classes_preds(net, images, labels):
+    '''
+    Generates matplotlib Figure using a trained network, along with images
+    and labels from a batch, that shows the network's top prediction along
+    with its probability, alongside the actual label, coloring this
+    information based on whether the prediction was correct or not.
+    Uses the "images_to_probs" function.
+    '''
+    preds, probs = images_to_probs(net, images)
+    classes = ['cat','dog']
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(12, 48))
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx+1, xticks=[], yticks=[])
+        matplotlib_imshow(images[idx])
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            classes[preds[idx]],
+            probs[idx] * 100.0,
+            classes[labels[idx]]),
+                    color=("green" if preds[idx]==labels[idx].item() else "red"))
+    return fig
+
+def images_to_probs(net, images):
+    '''
+    Generates predictions and corresponding probabilities from a trained
+    network and a list of images
+    '''
+    output = net(images)
+    # convert output probabilities to predicted class
+    _, preds_tensor = torch.max(output, 1)
+    preds = np.squeeze(preds_tensor.cpu().numpy())
+    return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
+
+def train(epoch, model, train_loader, optimizer, writer, device):
+    model.train()
+    running_loss = 0.0
+    for batch_idx, (data, target) in tqdm(enumerate(train_loader)):
+        
+        data, target = data.to(device), target.to(device)
+        
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+        running_loss += loss.item()
         if batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+            writer.add_scalar('training loss',
+                            running_loss / 1000,
+                            epoch * len(train_loader) + batch_idx)
+
+            # random mini-batch
+            writer.add_figure('predictions vs. actuals',
+                            plot_classes_preds(model, data, target),
+                            global_step=epoch * len(train_loader) + batch_idx)
             
-def test(model, valid_loader):
+def test(model, valid_loader, device):
     model.eval()
     test_loss = 0
     correct = 0
     for data, target in valid_loader:
         # permute pixels
+        data, target = data.to(device), target.to(device)
         output = model(data)
         test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss                                                               
         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability                                                                 
@@ -28,7 +87,6 @@ def test(model, valid_loader):
 
     test_loss /= len(valid_loader.dataset)
     accuracy = 100. * correct / len(valid_loader.dataset)
-    accuracy_list.append(accuracy)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(valid_loader.dataset),
         accuracy))
